e62582c0dd6609a1c113052a8d157a5f22d02723
justin <justinpolacek@gmail.com>
2020-09-25 12:03:22 -0700

    first

---
+++ server/controller.js
@@ -0,0 +1,44 @@
+const movies = [
+    { id: 0,
+      name: 'Tenet',
+      date: '2020'
+
+
+     },
+     
+
+     {
+        id: 1,
+        name: 'Knocking on Heavens Door',
+        date: '1990'
+
+     },
+     {
+        id: 2,
+        name: 'Stalker',
+        date: '1979'
+
+     },
+     
+     {
+        
+        id: 3,
+        name: 'Inception',
+        date: '2010'
+
+     },
+     {
+        id: 4,
+        name: 'Interstellar',
+        date: '2014'
+
+     },
+     {
+         id: 5,
+         name: 'The Matrix',
+         date: '1999`'
+
+     }
+
+     
+]
