099077925cd513fe0e6e9998a0cdc041b9c0cfd8
justin <justinpolacek@gmail.com>
2020-09-28 22:00:40 -0700

    not finished

--- project/server/controller.js
+++ project/server/controller.js
@@ -15,13 +15,19 @@ const movies = [
         id: 2,
         name: 'Knocking on heavens door',
         date: '1990',
-        img: 'https://m.media-amazon.com/images/M/MV5BMTk2MjcxNjMzN15BMl5BanBnXkFtZTgwMTE3OTEwNjE@._V1_UY1200_CR135,0,630,1200_AL_.jpg'
+        img: 'https://images-na.ssl-images-amazon.com/images/I/71rNJQ2g-EL._AC_SL1178_.jpg'
     },
     {
         id: 3,
         name: 'Terminator',
         date: '1985',
-        img: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Terminator1984movieposter.jpg/220px-Terminator1984movieposter.jpg'
+        img: 'https://cdn.shopify.com/s/files/1/0057/3728/3618/products/4c177c2b7f7bb9a679f089bcb50f844e_3e89eb5d-ffbd-4033-a00f-e595a3ef2e2a_240x360_crop_center.progressive.jpg?v=1573587540'
+    },
+    {
+        id: 4,
+        name: 'Ad Astra',
+        date: '2019',
+        img: 'https://specials-images.forbesimg.com/imageserve/5d7bcd740762110007967b6e/960x0.jpg?fit=scale'
     }
 ];
 
