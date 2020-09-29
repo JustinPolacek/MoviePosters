ea68753923692ade0e059b6634f3d602891ad8e0
justin <justinpolacek@gmail.com>
2020-09-28 21:49:54 -0700

    not finished

--- project/src/Component/Header.js
+++ project/src/Component/Header.js
@@ -7,6 +7,12 @@ class Header extends Component{
             <h1 className="head">
                 NETFLIX
             </h1>
+            <nav>
+                <a href="#"><li>Movies</li></a>
+                <a href="#"><li>Titles</li></a>
+                <a href="#"><li>My List</li></a>
+                <a href="#"><li>TV Shows</li></a>
+            </nav>
         </div>
     )
 }
