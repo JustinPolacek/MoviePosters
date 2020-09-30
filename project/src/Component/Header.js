aec69b8c0b893935c693cdbfc7abf357441bf3d9
justin <justinpolacek@gmail.com>
2020-09-29 21:46:32 -0700

    not finished

--- project/src/Component/Header.js
+++ project/src/Component/Header.js
@@ -4,6 +4,7 @@ class Header extends Component{
     render(){
     return (
         <div>
+            <header>
             <h1 className="head">
                 <img src="https://imgix.bustle.com/uploads/image/2017/8/29/c8c8077a-10fc-44d5-93f0-da4e592a299e-netflix-logo-print_pms.jpg?w=1200&h=630&q=70&fit=crop&crop=faces&fm=jpg"></img>
             </h1>
@@ -13,6 +14,7 @@ class Header extends Component{
                 <a href="#"><li>My List</li></a>
                 <a href="#"><li>TV Shows</li></a>
             </nav>
+            </header>
         </div>
     )
 }
